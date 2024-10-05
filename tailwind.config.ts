2c1407860f9ff1f5a214dbb61525fd2a57e7bb80
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-10-05 22:00:08 +0900

    chore: color token 생성

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,18 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "gray-500": "#1E1E1E",
+        "gray-400": "#787878",
+        "gray-300": "#BBBBBB",
+        "gray-200": "#E8E8E8",
+        "gray-100": "#F3F3F3",
+        "green-600": "#BCDB00",
+        "green-500": "#DFFF00",
+        "green-400": "#E9FF3F",
+        "green-300": "#EFFF66",
+        "green-200": "#F6FF99",
+        "green-100": "#FBFFCC",
+        red: "#FF4A4A",
       },
     },
   },
